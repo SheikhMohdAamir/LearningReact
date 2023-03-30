c1a2a87d167f3506d281eafbd7b87922d48aeba8
Sheikh Mohd Aamir <jok20001+work@gmail.com>
2023-03-30 17:00:05 +0530

    Added Items To Expense List

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+import ExpenseList from "./components/ExpenseList";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem/>
+      <ExpenseList/>
+    </div>
+  );
+}
+export default App;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,9 @@
+function ExpenseList(){
+    return <ul>
+        <li>Food Rs10</li>
+        <li>Petrol Rs100</li>
+        <li>Movies Rs200</li>
+    </ul>
+}
+
+export default ExpenseList;
