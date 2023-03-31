8eef54f4f196c0c0b5274a4f6fd5453253d57039
Sheikh Mohd Aamir <jok20001+work@gmail.com>
2023-03-31 14:35:55 +0530

    Added CSS for Styling in ExpenseItem.js

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
+import "./ExpenseItem.css";
+
 function ExpenseItem(){
-    return <h2>Expense Item!</h2>
+    return <div className="expense-item">
+        <div>Date</div>
+        <div className="expense-item__description">
+            <h2>Expense Item</h2>
+            <div className="expense-item__price">Rs100</div>
+        </div>
+    </div>
 }
 
-export default ExpenseItem;
+export default ExpenseItem;    
