f0970cb09889e8a573c276830948083df8802c8b
Sheikh Mohd Aamir <jok20001+work@gmail.com>
2023-04-02 02:21:03 +0530

    Used looping to iterate through every element inside expense(Array of Objects)

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,14 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem(){
+function ExpenseItem(props){
+ 
     return <div className="expense-item">
-        <div>Date</div>
+        <div>{props.date.toDateString()}</div>
         <div className="expense-item__description">
-            <h2>Expense Item</h2>
-            <div className="expense-item__price">Rs100</div>
+            <h2>{props.title}</h2>
+            {/* <h3>{location}</h3> */}
+            <h2>{props.location}</h2>
+            <div className="expense-item__price">Rs {props.amount}</div>
         </div>
     </div>
 }
--- src/components/ExpenseList.js
+++
@@ -1,9 +0,0 @@
-function ExpenseList(){
-    return <ul>
-        <li>Food Rs10</li>
-        <li>Petrol Rs100</li>
-        <li>Movies Rs200</li>
-    </ul>
-}
-
-export default ExpenseList;
