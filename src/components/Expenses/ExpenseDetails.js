a92d7bb4fc1b3734a53e630224ce584329e61628
Sheikh Mohd Aamir <jok20001+work@gmail.com>
2023-04-03 03:50:14 +0530

    Delete Button Added

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,9 +1,12 @@
+import "./Button.css";
+
 const ExpenseDetails=(props)=>{
     return(
         <div className="expense-item__description">
             <h2>{props.title}</h2>
             <h2>{props.location}</h2>
             <div className="expense-item__price">Rs {props.amount}</div>
+            <button className="button">Delete</button>
         </div>
     )
 }
