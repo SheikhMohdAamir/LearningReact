00872d38607c4e9044861cfa02a06319ce222a3b
Sheikh Mohd Aamir <jok20001+work@gmail.com>
2023-04-03 15:42:47 +0530

    "Change" Button Added

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,12 +1,24 @@
+import {useState} from "react";
 import "./Button.css";
 
 const ExpenseDetails=(props)=>{
+    const [amount, setAmount] = useState(props.amount);
+    const changeHandler = ()=>{
+        setAmount(100);
+    }
+
+    const [title, setTitle] = useState(props.title);
+    const clickHandler = ()=>{
+        setTitle("Updated");
+    }
+
     return(
         <div className="expense-item__description">
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <h2>{props.location}</h2>
-            <div className="expense-item__price">Rs {props.amount}</div>
-            <button className="button">Delete</button>
+            <div className="expense-item__price">Rs {amount}</div>
+            <button className="button2" onClick={changeHandler}>Change</button>
+            <button className="button" onClick={clickHandler}>Delete</button>
         </div>
     )
 }
