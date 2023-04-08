dea1c40b562eaf3c837e22ce3ba5b93eadb9b7cb
Sheikh Mohd Aamir <jok20001+work@gmail.com>
2023-04-08 06:10:57 +0530

    Console Logging Form Details

--- src/components/Expenses/ExpenseForm.js
+++ src/components/Expenses/ExpenseForm.js
@@ -1,45 +1,55 @@
 import React,{ useState } from "react";
 import "./ExpenseForm.css";
-
 function ExpenseForm(){
 
-    // On Submission of the form the input details will get console logged. And Add this(onSubmit={def}) to form tag to run the code.
-    // const def= (event)=>{
-    //     event.preventDefault();
+    // ENTERED TITILE AND THEN UPDATING TITLE - Add these to input tags, onChange={titleHandler}, onChange={amountHandler}, onChange={locationHandler}, onChange={dateHandler}.
+    const [enteredTitle, updatedTitle] = useState();
+    const [enteredAmount, updatedAmount] = useState();
+    const [enteredLocation, updatedLocation] = useState();
+    const [enteredDate, updatedDate] = useState();
 
-    //     // Creating  variables of input values
-    //     const title= document.getElementById("expensetitle").value;
-    //     const amount= document.getElementById("expenseamount").value;
-    //     const date= document.getElementById("date").value;
+    const titleHandler = (event)=>{
+        updatedTitle(event.target.value)
+    }
 
-    //     console.log(`Expense Title - ${title}, Expense Amount - ${amount}, Date - ${date}`);
-    //     }
+    const amountHandler = (event)=>{
+        updatedAmount(event.target.value)
+    }
 
-        const [title, updatedTitle] = useState();
-        const [amount, updatedAmount] = useState();
-        const [date, updatedDate] = useState();
-    
-        const titleHandler = (event)=>{
-            console.log(updatedTitle(event.target.value))
+    const locationHandler = (event)=>{
+        updatedLocation(event.target.value)
+    }
+    const dateHandler = (event)=>{
+        updatedDate(event.target.value)
+    }
+        
+    // On Submission of the form the input details will get console logged. And Add this(onSubmit={submitHandler}) to form tag to run the code.
+    const submitHandler= (event)=>{
+        event.preventDefault();
+
+        const formData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            location: enteredLocation,
+            date: enteredDate
         }
-
-        const amountHandler = (event)=>{
-            console.log(updatedAmount(event.target.value))
-        }
-
-        const dateHandler = (event)=>{
-            console.log(updatedDate(event.target.value))
+        console.log(formData);
+        
+        
         }
         
     return(
         <div className="formdiv">
         <h2 className="formh2">Expense Form</h2>
-        <form > 
+        <form onSubmit={submitHandler}> 
             <label for="expensetitle">Expense Title</label>
             <input type="text" name="expensetitle" id="expensetitle" className="input" placeholder="Enter the title..." onChange={titleHandler}/>
             <br></br>
             <label for="expenseamount">Expense Amount</label>
-            <input type="number" name="expenseamount" id="expenseamount" className="input" placeholder="Enter the amount..." onChange={amountHandler}/>
+            <input type="number" name="expenseamount" id="expenseamount" className="input" placeholder="Enter the amount..."  onChange={amountHandler}/>
+            <br></br>
+            <label for="expenselocation">Location</label>
+            <input type="text" name="expenselocation" id="expenselocation" class="input" placeholder="Enter the location..." onChange={locationHandler}/>
             <br></br>
             <label for="date">Date</label>
             <input type="date" name="date" id="date" className="input" onChange={dateHandler}/>
